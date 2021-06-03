50e4c5244865959ae5dbe1d3de720031fdba9a67
ayatab <42857860+ayatab@users.noreply.github.com>
2021-06-03 03:23:32 -0700

    re-pasted the original R documents

--- final/server.R
+++ final/server.R
@@ -7,8 +7,9 @@
 #    http://shiny.rstudio.com/
 #
 
-## Load libraries
 library(shiny)
+library(maps)
+library(tidyverse)
 
 country_vaccinations <- read.csv("../data/country_vaccinations.csv")
 country_by_manufacturer <- read.csv("../data/country_vaccinations_by_manufacturer.csv")
@@ -16,46 +17,51 @@ county_statistics <- read.csv("../data/county_statistics.csv")
 trump_biden <- read.csv("../data/trump_biden_polls.csv")
 trump_clinton <- read.csv("../data/trump_clinton_polls.csv")
 vaccine_hesitancy <- read.csv("../data/Vaccine_Hesitancy_County.csv")
-vaccine_hesitancy_state <-read.csv("../data/Data_with_state_vaccine")
-
-US_data <- country_vaccinations %>%
-    filter(country == "United States", na.rm = TRUE) %>%
-    filter(date >= as.Date("2021-01-01")) %>%
-    filter(!is.na(people_vaccinated))
-    
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
-    ## To filter by date in UI
-    filterByDate <- reactive({
-        US_data %>%
-            filter(date %in% input$date) %>%
-            group_by(date) 
-    })
     
-    ## filtered by type(rates)
+    ## load the county shapefile and join on county fips
+    county_shapes <- map_data("county") %>%
+        # load county boundary data (package "maps")
+        unite(polyname, region, subregion, sep = ",") %>%
+        # put the polygon name in the same form as in county.fips
+        left_join(county.fips, by = "polyname")
+    # merge with fips data
     
-            
+    # combine county shapes with vaccine hesitancy data
+    map_hesitancy <- left_join(vaccine_hesitancy, county_shapes, by = c("FIPS.Code" = "fips"))
     
-    
-    
-    
-    
-    
-    output$distPlot <- renderPlot({
+    output$mapPlot <- renderPlot({
         
-
-
-
+        if (input$mapFilter == "Estimated % Hesitant") {
+            # Map displayed if user selects Estimated % Hesitant and appropriate labels/scale
+            ggplot(map_hesitancy, aes(x = long, y = lat)) +
+                geom_polygon(aes(group = group, fill = Estimated.hesitant), color = "black") +
+                scale_fill_gradient(low = "white", high = "red", breaks = c(0,0.5) ,limits=c(0, 0.5), labs(scale = "% of Population")) +
+                labs(title = "Estimated % Hesitant") +
+                coord_quickmap()
+        } else if (input$mapFilter == "Estimated % Strongly Hesitant") {
+            # Map displayed if user selects Estimated % Strongly Hesitant and appropriate labels/scale
+            ggplot(map_hesitancy, aes(x = long, y = lat)) +
+                geom_polygon(aes(group = group, fill = Estimated.strongly.hesitant), color = "black") +
+                scale_fill_gradient(low = "white", high = "red", breaks = c(0,0.5) ,limits=c(0, 0.5), labs(scale = "% of Population")) +
+                labs(title = "Estimated % Strongly Hesitant") +
+                coord_quickmap()
+        } else if (input$mapFilter == "Estimated % Hesitant or Unsure") {
+            # Map displayed if user selects Estimated % Hesitant or Unsure and appropriate labels/scale
+            ggplot(map_hesitancy, aes(x = long, y = lat)) +
+                geom_polygon(aes(group = group, fill = Estimated.hesitant.or.unsure), color = "black") +
+                scale_fill_gradient(low = "white", high = "red", breaks = c(0,0.5) ,limits=c(0, 0.5), labs(scale = "% of Population")) +
+                labs(title = "Estimated % Hesitant or Unsure") +
+                coord_quickmap()
+        } else if (input$mapFilter == "% Fully Vaccinated") {
+            # Map displayed if user selects % Fully Vaccinated and appropriate labels/scale
+            ggplot(map_hesitancy, aes(x = long, y = lat)) +
+                geom_polygon(aes(group = group, fill = Percent.adults.fully.vaccinated.against.COVID.19), color = "black") +
+                scale_fill_gradient(low = "white", high = "blue", breaks = c(0,0.5,1) ,limits=c(0, 1), labs(scale = "% of Population")) +
+                labs(title = "Estimated % Fully Vaccinated") +
+                coord_quickmap()
+        }
     })
-    
-    ## Rendering output message for plot information
-    ##output$message <- renderText({
-    ##    output_msg <- paste("In", input$states, ", there were", filterByTime_AM()$AM_count[1], "UFO sightings in the AM hours as compared to ",
-    ##                        filterByTime_PM()$PM_count[1], "in the PM hours.")
-    ##})
-
 })
-
-
