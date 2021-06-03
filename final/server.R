b45e98daa0ac5dc86029d96e16437a21e98a96ef
Justin Tham <jtham@uw.edu>
2021-06-03 01:02:13 -0700

    implemented test map in server

--- final/server.R
+++ final/server.R
@@ -8,6 +8,8 @@
 #
 
 library(shiny)
+library(maps)
+library(tidyverse)
 
 country_vaccinations <- read.csv("../data/country_vaccinations.csv")
 country_by_manufacturer <- read.csv("../data/country_vaccinations_by_manufacturer.csv")
@@ -19,15 +21,22 @@ vaccine_hesitancy <- read.csv("../data/Vaccine_Hesitancy_County.csv")
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    ## load the county shapefile and join on county fips
+    county_shapes <- map_data("county") %>%
+                        # load county boundary data (package "maps")
+        unite(polyname, region, subregion, sep = ",") %>%
+                        # put the polygon name in the same form as in county.fips
+        left_join(county.fips, by = "polyname")
+                        # merge with fips data
+    
+    map_hesitancy <- left_join(vaccine_hesitancy, county_shapes, by = c("FIPS.Code" = "fips"))
+    
+    output$mapPlot <- renderPlot({
+        
+        ggplot(map_hesitancy, aes(x = long, y = lat)) +
+            geom_polygon(aes(fill = Percent.Hispanic), color = "black") +
+            scale_fill_gradient2(low = "blue", mid = "white", high = "red", midpoint = 0.5) +
+            coord_quickmap()
+        
     })
-
 })
